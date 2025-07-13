5aa470a488d79d5d42a6179d961e7a34a06b0541
Elaine Gibson <ypsvlq@gmail.com>
2025-07-13 13:30:10 +0100

    update for zig master

--- build.zig
+++ build.zig
@@ -11,9 +11,11 @@ pub fn build(b: *std.Build) void {
     });
 
     const unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/win32.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/win32.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
