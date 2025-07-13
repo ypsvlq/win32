e9a8067633e82544c4cf69ca24b980e3bf999732
Elaine Gibson <ypsvlq@gmail.com>
2025-07-13 22:23:16 +0100

    remove test step

--- build.zig
+++ build.zig
@@ -1,31 +1,15 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
     _ = b.addModule("win32", .{
         .root_source_file = b.path("src/win32.zig"),
-        .target = target,
-        .optimize = optimize,
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
     });
-
-    const unit_tests = b.addTest(.{
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/win32.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
-    });
-
-    const run_unit_tests = b.addRunArtifact(unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
 
     const generate = b.addSystemCommand(&.{ "dotnet", "run", "--project", "generator" });
     generate.addFileArg(b.path("src/win32.zig"));
 
-    const generate_step = b.step("generate", "Generate src/win32.zig");
+    const generate_step = b.step("generate", "Generate win32.zig");
     generate_step.dependOn(&generate.step);
 }
